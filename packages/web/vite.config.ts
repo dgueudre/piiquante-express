08b4cba874e18db9b365009209a54164f67497f1
dgueudre <dgueudre@gmail.com>
2024-09-29 15:29:43 +0200

    fix: run web with validations from shared

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://github.com/vitejs/vite/discussions/14172
+  optimizeDeps: { include: ['@piiquante/shared'] },
   plugins: [react()],
 });
