58b56abf06dcb6943829bb92d492838858f4e3a3
dgueudre <dgueudre@gmail.com>
2024-09-29 19:13:02 +0200

    fix build && watch shared

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   // https://github.com/vitejs/vite/discussions/14172
-  optimizeDeps: { include: ['@piiquante/shared'] },
+  // optimizeDeps: { include: ['@piiquante/shared'] },
   plugins: [react()],
 });
